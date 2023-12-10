2f43f3d399c78909d5d904e0bb9cc5fcc4d4a181
samuel21695 <samuel21695@gmail.com>
2023-12-10 19:02:28 +0900

    ✨ usage example

--- constructor.ts
+++ constructor.ts
@@ -1,4 +1,4 @@
-class Calculaator {
+class Calculator {
   private _a: number;
   private _b: number;
 
@@ -65,4 +65,26 @@ class Calculaator {
   }
 }
 
+// 사용 예
+const calc = new Calculator(10, 5);
+console.dir(Calculator); //?
+/**
+ * 생성자 함수의 구조를 위의 console.dir()로 확인하면 아래의 '구조'를 확인할 수 있습니다.
+ * static, 즉 정적 메서드는 생성자 함수의 프로퍼티로 붙어있는 것이 아니라, 생성자 함수 자체의 프로퍼티로 붙어있는 것을 확인할 수 있습니다.
+ * static(정적이다) 라는 해당 표현의 의미와 public 키워드의 차이는
+ * 정적 메서드는 생성자 함수의 인스턴스를 생성하지 않고도 사용할 수 있지만, public 메서드는 인스턴스를 생성해야만 사용할 수 있다는 것입니다.
+ * 우리가 흔히 사용하고 봐왔던 메서드는 public 메서드입니다
+ * static 메서드와 매우 혼동되는 부분이므로 Calculator.addStatic() 메서드는 new 키워드를 사용하지 않고서도 사용할 수 있습니다.
+ {
+  [λ: Calculator]
+    addStatic: [λ],
+    subtractStatic: [λ],
+    multiplyStatic: [λ],
+    divideStatic: [λ]
+ }
+ */
 
+console.log(calc.add()); // 15
+console.log(calc.subtract()); // 5
+console.log(calc.multiply()); // 50
+console.log(Calculator.addStatic(10, 5)); // 15 (static 메서드 사용)
